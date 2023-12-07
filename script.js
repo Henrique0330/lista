94c3f7094648a45c3bb5e682d2040f1eadfaacac
Henrique0330 <henriquebelboni@gmail.com>
2023-12-06 23:38:19 -0300

    Acabei amém

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function adicionar(){
-    let text = document.getElementById("texto").value
-    let list = document.getElementById("Lista").innerHTML
+let Button = document.getElementById("adicionar-tarefa");
+let tarefasA = document.getElementById("tarefasA");
+let inputcamp = document.getElementById("Resultado");
+let Complete = document.getElementById("TCompletas");
 
-    list = + list + "<li>" +text+ "</li>"
+function additem() {
+    let terminando = document.createElement("p");
+    let buttonFinish = document.createElement("button")
+    buttonFinish.textContent = "Finish"
+    inputcamp.focus();
+    terminando.innerText = inputcamp.value;
+    tarefasA.appendChild(terminando);
+    terminando.appendChild(buttonFinish)
+    inputcamp.value = ""
+    terminando.addEventListener("click", function () {
+        tarefasA.removeChild(terminando);
+        Complete.appendChild(terminando);
+        terminando.removeChild(terminando.childNodes[1])
+        inputcamp.focus();
+    })
+    document.getElementById("limpar").addEventListener("click", function () {
+        document.getElementById("TCompletas").removeChild(terminando);
+        inputcamp.focus();
+    })
 }
+Button.addEventListener("click", additem)
+inputcamp.addEventListener("keyup", function (e) {
+    if (e.keyCode == 13) {
+        additem()
+    }
+
+
+})
